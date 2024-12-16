5b9e811b9be7e637b029531db842443d90094753
admin <you@example.com>
2024-12-16 13:58:36 +0100

    delete and update operation have been added

---
+++ src/Services/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductHandler(
+    IDocumentSession session, 
+    ILogger<DeleteProductHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"DeleteProductHandler.Handle called with ProductId - {request.Id}");
+        
+        session.Delete<Product>(request.Id);
+
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
---
+++ src/Services/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,43 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(
+    Guid Id,
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductCommandHandler(
+    IDocumentSession session, 
+    ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"UpdateProductCommandHandler.Handle called");
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException("Product was not found");
+        }
+
+        product.Name = command.Name;
+        product.Description = command.Description;
+        product.Category = command.Category;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+        
+        session.Update(product);
+
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateProductResult(true);
+    }
+}
