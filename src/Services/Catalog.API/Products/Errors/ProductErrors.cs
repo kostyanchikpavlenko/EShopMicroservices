385fcc9b8f4963b940aa2bec46ff12b63cb55803
admin <you@example.com>
2024-12-18 16:46:05 +0100

    Logging has been added as part of Mediatr pipeline

---
+++ src/Services/Catalog.API/Products/Errors/ProductErrors.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.Result;
+
+namespace Catalog.API.Products.Errors;
+
+public static class ProductErrors
+{
+    public static Error ProductNotFound = new("GetProductByIdHandler.Handle", "Product was not found");
+}
